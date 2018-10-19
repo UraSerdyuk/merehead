63600158931eecddc5ee837bf8281cefb4534a42
Yura <yura@Yuras-MacBook-Air.local>
2018-10-19 22:13:55 +0300

    first comit

---
+++ src/Components/test.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+class Test extends Component {
+    render() {
+        return(
+            <div>
+                test message
+            </div>
+        );
+    }
+}
+export default Test;
