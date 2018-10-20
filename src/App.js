d6c1c10b4476abf0abb165fc23fa26e896f11476
Yura <cb8593bc@gmail.com>
2018-10-20 23:45:49 +0300

    add fetch

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Test from './Components/test'
 class App extends Component {
--- src/Components/test.js
+++ src/Components/test.js
@@ -1,11 +1,43 @@
 import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
 
 class Test extends Component {
+
+    constructor(props) {
+        super(props);
+    
+        this.state = { posts : [] };
+        this.state ={isOpened:false};
+      }
+
+    click() {
+        console.log(this.state.posts[0].name);
+
+
+    }
+    componentDidMount() {
+    fetch('http://dev.frevend.com/json/users.json')
+    .then(function(response){
+       return  response.json();
+
+    }).then(function(data){
+
+        let arr = [];
+             data.users.forEach(function(i) {
+                 arr.push(i);
+             });
+             return arr;
+         }).then((posts)=> {
+             console.log(posts);
+             this.setState({ posts });
+         })
+  
+} 
+
     render() {
         return(
-            <div>
+            <div onClick={this.click.bind(this)}>
                 test message
+
             </div>
         );
     }
