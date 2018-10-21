c1e6fc42adbc2d1ac9f901789b913feb68ee5d2b
Yura <yura@Yuras-MacBook-Air.local>
2018-10-21 11:57:51 +0300

    add pagination

--- src/Components/test.js
+++ src/Components/test.js
@@ -1,14 +1,22 @@
 import React, { Component } from 'react';
 
+import Pagination from "react-js-pagination";
+import Loader from 'react-loader-spinner'
 class Test extends Component {
 
     constructor(props) {
         super(props);
     
-        this.state = { posts : [] };
+        this.state = { 
+            posts : [],
+            activePage: 15
+         };
         
       }
-
+      handlePageChange(pageNumber) {
+        console.log(`active page is ${pageNumber}`);
+        this.setState({ activePage: pageNumber });
+      }
    
     componentDidMount() {
     fetch('http://dev.frevend.com/json/users.json')
@@ -32,6 +40,12 @@ class Test extends Component {
 render() {
     return(
         <div >
+            <Loader 
+         type="Puff"
+         color="#00BFFF"
+         height="100"	
+         width="100"
+      />  
            {this.state.posts.map((key)=>{
                 return <div key={key.id}>
                      <p>{key.name}</p>
@@ -39,6 +53,13 @@ render() {
                      <p>{key.desc}</p>
                 </div>
             })}
+            <Pagination
+          activePage={this.state.activePage}
+          itemsCountPerPage={10}
+          totalItemsCount={450}
+          pageRangeDisplayed={5}
+          onChange={this.handlePageChange}
+        />
             
 
         </div>
