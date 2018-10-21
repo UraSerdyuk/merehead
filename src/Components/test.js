af02b95490f8d690418a6563d8313fb2192b91ab
Yura <yura@Yuras-MacBook-Air.local>
2018-10-22 00:50:07 +0300

    add style

--- src/Components/test.js
+++ src/Components/test.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { createStore } from 'redux'
 import Pagination from "react-js-pagination";
 import Loader from 'react-loader-spinner'
+import styled from 'styled-components';
+import '././css/style.css';
 class Test extends Component {
     constructor(props) {
         super(props);
@@ -11,7 +13,9 @@ class Test extends Component {
             activePage: 0,
             preloader: true
          };
+ 
       }
+      
       
       handlePageChange(pageNumber) {
         console.log(`active page is ${pageNumber}`);
@@ -40,68 +44,96 @@ class Test extends Component {
 } 
 
 render() {
-    return(
-                <div >
+const LoaderStyle = styled.div`
+            text-align: center;
+            padding: 22%;
+    
+`;
+const Paragraf = styled.p`
+            text-align: center;
+            font-size: 30px;
+            padding-top: 4%;
+    
+`;
+const Card = styled.div`
+display: flex;
+margin: auto;
+            padding: 2%;
+            border: 1px solid black;
+            width: 500px;
+            border-radius: 18px;
+            margin-bottom: 100px;
+`;
 
-                {(this.state.preloader)? <Loader type="Bars" color="#somecolor" height={80} width={80} /> : <div>
-                <p> testing </p>
-                <Pagination
-                    activePage={this.state.activePage}
-                    itemsCountPerPage={5}
-                    totalItemsCount={20}
-                    pageRangeDisplayed={5}
-                    onChange={this.handlePageChange.bind(this)}
-                />
+
+
+    return(
+                <div  className='main'>
+
+                {(this.state.preloader)? <LoaderStyle>
+                                                <Loader type="Bars" color="#somecolor" height={100} width={100} />
+                                        </LoaderStyle>: <div>
+                                        <Paragraf> testing work   front-end developer  Merehead </Paragraf>
+                                       
+                                        <Pagination 
+                                        
+                                            activePage={this.state.activePage}
+                                            itemsCountPerPage={5}
+                                            totalItemsCount={20}
+                                            pageRangeDisplayed={5}
+                                            onChange={this.handlePageChange.bind(this)}
+                                        />
+                                       
                 </div> }
 
                 {(this.state.activePage === 1)?<div>
                     {this.state.posts.map((key)=>{
                         if(key.id >0 && key.id <=5 ){
-                            return <div key={key.id}>
+                            return <Card key={key.id}>
                             <p>{key.id}</p>
                             <p>{key.name}</p>
                             <p>{key.surname}</p>
                             <p>{key.desc}</p>
-                        </div>
+                        </Card>
                         }
                     
                   })}
                 </div> : ''}
                 {(this.state.activePage === 2)?<div>
                     {this.state.posts.map((key)=>{
-                        if(key.id >=5 && key.id <=10 ){
-                            return <div key={key.id}>
+                        if(key.id >5 && key.id <=10 ){
+                            return <Card key={key.id}>
                             <p>{key.id}</p>
                             <p>{key.name}</p>
                             <p>{key.surname}</p>
                             <p>{key.desc}</p>
-                        </div>
+                        </Card>
                         }
                     
                   })}
                 </div> : ''}
                 {(this.state.activePage === 3)?<div>
                     {this.state.posts.map((key)=>{
-                        if(key.id >=10 && key.id <=15 ){
-                            return <div key={key.id}>
+                        if(key.id >10 && key.id <=15 ){
+                            return <Card key={key.id}>
                             <p>{key.id}</p>
                             <p>{key.name}</p>
                             <p>{key.surname}</p>
                             <p>{key.desc}</p>
-                        </div>
+                        </Card>
                         }
                     
                   })}
                 </div> : ''}
                 {(this.state.activePage === 4)?<div>
                     {this.state.posts.map((key)=>{
-                        if(key.id >=15 && key.id <=20 ){
-                            return <div key={key.id}>
+                        if(key.id >15 && key.id <=20 ){
+                            return <Card key={key.id}>
                             <p>{key.id}</p>
                             <p>{key.name}</p>
                             <p>{key.surname}</p>
                             <p>{key.desc}</p>
-                        </div>
+                        </Card>
                         }
                     
                   })}
